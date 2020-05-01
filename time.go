b280c88b1d178445d1d3e7c56f52899462708c9f
JustinTan <mrnatande@gmail.com>
2020-05-01 15:54:33 +0800

    Parse time

--- time.go
+++ time.go
@@ -6,8 +6,14 @@ import (
 	"time"
 )
 
-var dateFormats = []string{
-	"2006-1-2", "20060102", "2006/1/2", "2/1/2006",
+var timeFormats = []string{
+	"2006-01-02T15:04:05Z07:00",
+	"2006-01-02T15:04:05",
+	"2006-01-02 15:04:05",
+	"2006-1-2",
+	"20060102",
+	"2006/1/2",
+	"2/1/2006",
 }
 
 // ToDuration converts an interface to a time.Duration type.
@@ -29,17 +35,17 @@ func ToDuration(i interface{}) (time.Duration, error) {
 	return 0, fmt.Errorf("cannot convert %#v of type %T to duration", i, i)
 }
 
-func ToDate(i interface{}) (*time.Time, error) {
-	return ToDateInLocation(i, time.Local)
+func ToTime(i interface{}) (*time.Time, error) {
+	return ToTimeInLocation(i, time.Local)
 }
 
-func ToDateInLocation(i interface{}, loc *time.Location) (*time.Time, error) {
+func ToTimeInLocation(i interface{}, loc *time.Location) (*time.Time, error) {
 	i = indirect(i)
 	s, err := ToString(i)
 	if err != nil {
 		return nil, fmt.Errorf("cannot convert %#v of type %T to date: %w", i, i, err)
 	}
-	for _, df := range dateFormats {
+	for _, df := range timeFormats {
 		d, err := time.ParseInLocation(df, s, loc)
 		if err == nil {
 			return &d, nil
