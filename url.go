f344e5dc746930135031bfd9155a9363f602f294
nat <mrnatande@gmail.com>
2021-01-14 13:25:54 -0500

    Add more funcs

--- url.go
+++ url.go
@@ -2,9 +2,12 @@ package conv
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/url"
+	"path"
+	"strings"
 )
 
 func ToURLValues(i interface{}) (url.Values, error) {
@@ -39,4 +42,42 @@ func MustURLValues(i interface{}) url.Values {
 		log.Panic(err)
 	}
 	return v
+}
+
+func JoinURLPath(a ...string) string {
+	if len(a) == 0 {
+		return ""
+	}
+	p := path.Join(a...)
+	p = strings.Replace(p, ":/", "://", 1)
+	i := strings.Index(p, "://")
+	s := p
+	if i >= 0 {
+		s = p[i:]
+		l := strings.Split(s, "/")
+		for i, v := range l {
+			l[i] = url.PathEscape(v)
+		}
+		p = p[:i] + path.Join(l...)
+	}
+	return p
+}
+
+func ToURL(s string) (string, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", fmt.Errorf("parse: %w", err)
+	}
+	if u.Scheme == "" {
+		return "", errors.New("missing schema")
+	}
+	if u.Host == "" {
+		return "", errors.New("missing host")
+	}
+	return u.String(), nil
+}
+
+func IsURL(s string) bool {
+	u, _ := ToURL(s)
+	return u != ""
 }
